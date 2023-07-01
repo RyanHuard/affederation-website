06a285341fcb9539b99f23d9fe294e72dec3cc62
huardy12 <77092347+huardy12@users.noreply.github.com>
2023-06-30 22:53:40 -0400

    Created carousel component for schedule ticker

--- src/components/header/ticker/Ticker.jsx
+++ src/components/header/ticker/Ticker.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
-import Game from './game/Game'
+import { IconButton } from "@chakra-ui/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+
+import Game from "./game/Game";
+import Carousel from "../carousel/Carousel";
+
+import { useState, useEffect } from "react";
 
 const Ticker = () => {
   return (
-    <div className="h-20">
-      <Game />
-    </div>
-  )
-}
+    <Carousel>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>1SA vs ALA</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>2MEM vs STL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>3SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>4SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>5SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>6SA vs ALA</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>7MEM vs STL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>8SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>9SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>0SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>1SA vs ALA</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>2MEM vs STL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>3SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>4SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>5SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>6SA vs ALA</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>7MEM vs STL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>8SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>9SHR vs ORL</h1>
+      </div>
+      <div className="h-20 min-w-[160px] border-l-2 border-neutral-700">
+        <h1>0SHR vs ORL</h1>
+      </div>
+    </Carousel>
+  );
+};
 
-export default Ticker
+export default Ticker;
--- src/components/header/ticker/game/Game.jsx
+++ src/components/header/ticker/game/Game.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import { useGames } from './api/getGames'
-import { Spinner } from '@chakra-ui/react';
+import React from "react";
+import { useGames } from "./api/getGames";
+import { Spinner } from "@chakra-ui/react";
 
 const Game = () => {
   const gamesQuery = useGames();
 
-  if (gamesQuery.isLoading) {
-    return (
-        <div className="w-full h-20 flex justify-center items-center">
-          <Spinner />
-        </div>
-    )
-  }
+  //   if (gamesQuery.isLoading) {
+  //     return (
+  //         <div className="w-full h-20 flex justify-center items-center">
+  //           <Spinner />
+  //         </div>
+  //     )
+  //   }
 
   return (
-    <div>Game</div>
-  )
-}
+    <div className="flex">
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>1SA vs ALA</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>2MEM vs STL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>3SHR vs ORL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>4SHR vs ORL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>5SHR vs ORL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>6SA vs ALA</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>7MEM vs STL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>8SHR vs ORL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>9SHR vs ORL</h1>
+      </div>
+      <div className="h-20 w-40 border-l-2 border-neutral-700">
+        <h1>0SHR vs ORL</h1>
+      </div>
+    </div>
+  );
+};
 
-export default Game
+export default Game;
