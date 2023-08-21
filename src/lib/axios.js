f41f287738677963aba641cd7fd4740ccaa21fa7
unknown <ryanjhuard@gmail.com>
2023-08-21 16:09:11 -0500

    article conditional check

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,5 +2,5 @@ import axios from "axios";
 
 // Changes port
 export const api = axios.create({
-  baseURL: `${window.location.protocol}//${window.location.hostname}:5000/api`,
+  baseURL: `${window.location.protocol}//${window.location.hostname}/api`,
 });
