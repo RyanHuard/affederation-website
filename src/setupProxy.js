57848052579c3a8ee3169828aae6f969af0de471
unknown <ryanjhuard@gmail.com>
2023-08-21 14:57:12 -0500

    changed proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
+      target: 'https://stark-taiga-48721.herokuapp.com/',
       changeOrigin: true,
     })
   );
