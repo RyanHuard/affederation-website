85e4cf7ac56f818688e8e5b29229cef4a52a47ba
unknown <ryanjhuard@gmail.com>
2023-08-24 14:12:08 -0500

    box score fix

--- src/features/game/components/BoxScore.jsx
+++ src/features/game/components/BoxScore.jsx
@@ -114,7 +114,7 @@ const BoxScore = ({ boxScore, game }) => {
           const awayStats = boxScore?.[teams[awayIndex]]?.[position];
           const homeStats = boxScore?.[teams[homeIndex]]?.[position];
 
-          if (!(awayStats.length > 0)) {
+          if (!(awayStats?.length > 0)) {
             return <div className="flex h-screen w-full items-center justify-center bg-[#edeef2]">
             <Spinner size="lg" className="relative bottom-64 sm:bottom-86" />
           </div>
@@ -151,7 +151,7 @@ const BoxScore = ({ boxScore, game }) => {
 
       <div className="block max-w-fit rounded-sm bg-white pb-6 pt-6 drop-shadow lg:hidden">
         {Object.entries(categories).map(([position, data], index) => {
-          const awayStats = boxScore[teams[awayIndex]][position];
+          const awayStats = boxScore[teams[awayIndex]]?.[position];
 
           return (
             <div className="flex h-full px-4">
@@ -173,7 +173,7 @@ const BoxScore = ({ boxScore, game }) => {
         <div id="border" className="mt-4 h-px w-full bg-neutral-300" />
 
         {Object.entries(categories).map(([position, data], index) => {
-          const homeStats = boxScore[teams[awayIndex]][position];
+          const homeStats = boxScore[teams[awayIndex]]?.[position];
 
           return (
             <div className="flex h-full px-4">
